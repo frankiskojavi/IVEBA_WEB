04ee9108d33ff2a346070875315e9287ea5e5a2b
fhuitvk2024 <francisco.huit@visualk.com>
2024-09-29 12:47:32 -0600

    Agregar formualrio ME13
    Se agrega formulario ME13 (aun no está completo).

--- IVEBAWEB/IVEBA_Web_App/Models/ArchivoME13/ArchivoME13DTO.cs
+++ IVEBAWEB/IVEBA_Web_App/Models/ArchivoME13/ArchivoME13DTO.cs
@@ -1,9 +1,18 @@
-﻿namespace IVEBA_Web_App.Models.ArchivoME13
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+
+namespace IVEBA_Web_App.Models.ArchivoME13
 {
     public class ArchivoME13DTO
     {
-        public int Codigo { get; set; }
-        public string Nombre { get; set; }
-
+        public string codigoArchivo { get; set; }        
+        public int  año { get; set; }
+        public int mes { get; set; }
+        public string nombreArchivo { get; set; }
+        public int registrosProcesados{ get; set;}
+        public int registrosConError{get; set;}
+        public int detalle { get; set; }
+        public string nit { get; set; }
+        public int generaError { get; set; }
     }
 }
+ 
--- IVEBAWEB/IVEBA_Web_App/Pages/ArchivoME13/ArchivoME13.cshtml.cs
+++ IVEBAWEB/IVEBA_Web_App/Pages/ArchivoME13/ArchivoME13.cshtml.cs
@@ -1,6 +1,7 @@
 using IVEBA_Web_App.Models.ArchivoME13;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IVEBA_Web_App.Pages.ArchivoME13
 {
@@ -8,17 +9,16 @@ namespace IVEBA_Web_App.Pages.ArchivoME13
     {
         [BindProperty]
         public ArchivoME13DTO FormModel { get; set; }
+        public List<SelectListItem> Meses { get; set; }
+        public List<SelectListItem> Años { get; set; }
 
         public void OnGet()
         {
             // Inicializar valores al cargar el formulario
-            FormModel = new ArchivoME13DTO
-            {
-                Codigo = 100,   // Valor inicial para Codigo
-                Nombre = "Ejemplo Nombre"  // Valor inicial para Nombre
-            };
+            cargarInformacionDefault();
         }
 
+        /*
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
@@ -26,6 +26,63 @@ namespace IVEBA_Web_App.Pages.ArchivoME13
                 return Page();
             }
             return RedirectToPage("/Index");
+        }
+        */
+
+        public IActionResult OnPostGenerarArchivoME13()
+        {            
+            // Simulación de proceso
+            FormModel.registrosProcesados = 100; // Ejemplo de valores modificados
+            FormModel.registrosConError = 5;
+
+            // Retornar un resultado JSON con los datos procesados
+            return new JsonResult(new
+            {
+                success = true,
+                registrosProcesados = FormModel.registrosProcesados,
+                registrosConError = FormModel.registrosConError
+            });
+        }
+
+        public void cargarInformacionDefault(){ 
+            string codigoArchivo = "IVEME13";
+            int añoSistema = System.DateTime.Now.Year;
+            int mesSistema = System.DateTime.Now.Month;
+
+            Meses = new List<SelectListItem>
+            {
+                new SelectListItem("Enero", "1"),
+                new SelectListItem("Febrero", "2"),
+                new SelectListItem("Marzo", "3"),
+                new SelectListItem("Abril", "4"),
+                new SelectListItem("Mayo", "5"),
+                new SelectListItem("Junio", "6"),
+                new SelectListItem("Julio", "7"),
+                new SelectListItem("Agosto", "8"),
+                new SelectListItem("Septiembre", "9"),
+                new SelectListItem("Octubre", "10"),
+                new SelectListItem("Noviembre", "11"),
+                new SelectListItem("Diciembre", "12")
+            };
+            int currentYear = DateTime.Now.Year;
+            Años = new List<SelectListItem>();
+            for (int year = 2005; year <= currentYear; year++)
+            {
+                Años.Add(new SelectListItem(year.ToString(), year.ToString()));
+            }
+
+            FormModel = new ArchivoME13DTO
+            {
+                codigoArchivo = codigoArchivo,
+                año = añoSistema,
+                mes = mesSistema,
+                nombreArchivo = $"C:/{ codigoArchivo }/{ añoSistema }{ mesSistema.ToString("00") }BA.117",
+                registrosProcesados = 0, 
+                registrosConError = 0,
+                detalle = 0,
+                nit = "",
+                generaError = 0
+            };
         }
     }
 }
