d4328b3e88466b51cba99c8629d82c81fd632751
Francisoc HuitHuit <frankiskojavi@gmail.com>
2024-10-20 17:52:25 -0600

    Eliminar IVEBA_WEB

--- IVEBAWEB/IVEBA_API_Rest/obj/Debug/net8.0/IVEBA_API_Rest.AssemblyInfo.cs
+++ IVEBAWEB/IVEBA_API_Rest/obj/Debug/net8.0/IVEBA_API_Rest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IVEBA_API_Rest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3582c17ab71467c7910e499511821b4f3e8fdec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69859050883a566ca8c389b9918ed1794f6ed99")]
 [assembly: System.Reflection.AssemblyProductAttribute("IVEBA_API_Rest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IVEBA_API_Rest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
