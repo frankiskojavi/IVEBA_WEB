91a405dfef4cf9ebdd8ba069a61ef72e7d5cefab
Francisoc HuitHuit <francisco.huit@visualk.com>
2025-03-02 09:41:48 -0600

    inicio IVEBA05

--- IVEBAWEB/IVEBA_API_Rest/obj/Debug/net8.0/IVEBA_API_Rest.AssemblyInfo.cs
+++ IVEBAWEB/IVEBA_API_Rest/obj/Debug/net8.0/IVEBA_API_Rest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IVEBA_API_Rest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d6c3a20bceb57f1fe12d3c5e3a7f7231a3d10c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed4d2d25f257da9011c59e31702ec8aacf4af7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IVEBA_API_Rest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IVEBA_API_Rest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
